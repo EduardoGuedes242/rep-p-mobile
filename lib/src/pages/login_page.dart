559ae85e2b49182399f33bf0b0542d268a4deadd
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-07-18 12:54:07 -0300

    Ajustando Layout da tela de login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,9 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-import 'package:eg_custom_ui/eg_custom_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:rep_p_mobile/src/common/dados.dart';
-import 'package:rep_p_mobile/src/repositories/funcionarios_repository.dart';
-import 'package:rep_p_mobile/src/repositories/marcacao.dart';
 import 'package:rep_p_mobile/src/ui/cores.dart';
 
 class LoginPage extends StatefulWidget {
@@ -20,83 +16,67 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.sizeOf(context).height,
-            width: double.infinity,
-            color: PaletaCores.background,
-            child: Column(
+      body: Container(
+        color: PaletaCores.backgroundBlue,
+        child: Stack(
+          children: [
+            Column(
               children: [
-                SizedBox(height: 220),
-                EgTextFormWithLabel(
-                  label: 'Login',
-                  hint: 'Informe seu CPF',
-                  controller: cpfController,
-                ),
-                SizedBox(height: 30),
-                EgTextFormWithLabel(
-                  label: 'Senha',
-                  hint: 'Informe sua senha',
-                  controller: senhaController,
-                ),
-                SizedBox(height: 10),
-                SizedBox(
-                  width: MediaQuery.sizeOf(context).width * 0.9,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
+                SizedBox(height: 180),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height - 180,
+                  decoration: BoxDecoration(
+                    color: PaletaCores.backgroundWhite,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                    ),
+                  ),
+                  child: Column(
                     children: [
-                      Text(
-                        'Manter Conectado',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      Checkbox(
-                        value: false,
-                        onChanged: (value) {},
+                      Spacer(),
+                      Container(
+                        height: 60,
+                        width: double.infinity,
+                        child: Row(
+                          children: [
+                            Spacer(),
+                            Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                color: PaletaCores.backgroundWhiteSolid,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                ),
+                              ),
+                              child: Center(
+                                child: Image.asset(
+                                    'assets/images/logo_inforvix.png'),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 50),
-                EgButtonCustom(
-                  title: 'Registrar Marcação',
-                  color: PaletaCores.azulPrimario,
-                  function: () async {
-                    await FuncionarioRepository().login(
-                      cpf: cpfController.text,
-                      senha: senhaController.text,
-                    );
-
-                    if(DadosGlobais().token != '') {
-                      MarcacaoRepository().registrarMarcacao();
-                    } 
-
-                  },
-                ),
-                SizedBox(height: 30),
-                EgButtonCustom(
-                  title: 'Entrar no Sistema',
-                  color: PaletaCores.azulPrimario,
-                  function: () {},
-                ),
               ],
             ),
-          ),
-          Container(
-            height: 200,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: PaletaCores.azulPrimario,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(60),
+            Container(
+              color: PaletaCores.backgroundBlue,
+              width: double.infinity,
+              height: 180,
+              child: Center(
+                child: Image.asset(
+                  'assets/images/logo_pontovix.png',
+                  width: 150,
+                  height: 150,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/src/ui/cores.dart
+++ lib/src/ui/cores.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class PaletaCores {
-  static const Color background = Color(0XFFFBFFF1); 
-  static const Color azulPrimario = Color(0XFF3D52d5); 
+  static Color backgroundBlue = const Color(0XFF003472);
+  static Color backgroundWhite = const Color(0XFFF0F3FF);
+  static Color buttonColorBlue = const Color(0XFF016CF8);
+  static Color textButtonColorWhite = const Color(0XFFFFFFFF);
+  static Color backgroundWhiteSolid = const Color(0XFFFFFFFF);
+  static Color textColorBlack = const Color(0XFF000000);
 }
